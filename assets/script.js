fbf9ad23e8f349e24f3708458ce7ee98bb2ae8c4
uiido <uiido@yahoo.com>
2022-12-07 00:27:53 -0500

    Created var for each hour block

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,22 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 // Sets current date
 var today = dayjs();
 $('#currentDay').text(today.format('dddd MMM, D YYYY'));
 
-$(function () {
+// Sets variables for each time slot
+var hour9 = document.querySelector('#hour-9');
+var hour10 = document.querySelector('#hour-10');
+var hour11 = document.querySelector('#hour-11');
+var hour12 = document.querySelector('#hour-12');
+var hour13 = document.querySelector('#hour-13');
+var hour14 = document.querySelector('#hour-14');
+var hour15 = document.querySelector('#hour-15');
+var hour16 = document.querySelector('#hour-16');
+var hour17 = document.querySelector('#hour-17');
+
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+// $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -25,4 +35,4 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-});
+// });
